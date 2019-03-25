d07a2e6baef75e5a2974465cfd166a4b32ab1e18
Justin Gosses <jgosses82@gmail.com>
2019-03-25 17:55:21 -0500

    done

--- js/main.js
+++ js/main.js
@@ -106,8 +106,71 @@ var style = new PIXI.TextStyle({
     dropShadowDistance: 20,
 });
 
+var style2 = new PIXI.TextStyle({
+    fontFamily: 'Helvetica',
+    fontSize: 50,
+    fontWeight: 'bold',
+    fill: ['#000000'], // gradient
+    dropShadow: true,
+    dropShadowColor: '#c0c9d6',
+    dropShadowBlur: 8,
+    dropShadowAngle: Math.PI / 6,
+    dropShadowDistance: 20,
+});
+
+var style3 = new PIXI.TextStyle({
+    fontFamily: 'Helvetica',
+    fontSize: 25,
+    fontWeight: 'bold',
+    fill: ['#000000'], // gradient
+    dropShadow: true,
+    dropShadowColor: '#c0c9d6',
+    dropShadowBlur: 8,
+    dropShadowAngle: Math.PI / 6,
+    dropShadowDistance: 20,
+});
+
 var richText = new PIXI.Text('Happy Birthday', style);
 richText.x = window.innerWidth/2 - richText.getBounds().width/2;
-richText.y = window.innerHeight/2 - richText.getBounds().height/2;
+richText.y = window.innerHeight/6 - richText.getBounds().height/2;
 
 app.stage.addChild(richText);
+
+var richText2 = new PIXI.Text('Miles!', style);
+richText2.x = window.innerWidth/2 - richText2.getBounds().width/2;
+richText2.y = window.innerHeight/2.5 + richText.getBounds().height/1.5;
+
+app.stage.addChild(richText2);
+
+
+var richText3 = new PIXI.Text('from Uncle Justin & Aunt Yi yun', style3);
+richText3.x = window.innerWidth/2 - richText3.getBounds().width/2;
+richText3.y = window.innerHeight/1.7 + richText.getBounds().height/1.5;
+
+app.stage.addChild(richText3);
+
+var richText4 = new PIXI.Text("Go to Grandma Jo's house for a toy store gift certificate", style3);
+richText4.x = window.innerWidth/2 - richText4.getBounds().width/2;
+richText4.y = window.innerHeight/1.2 - richText.getBounds().height/11;
+
+app.stage.addChild(richText4);
+
+// var image = new PIXI.image(src="img/miles_w_mustache.png")
+// app.stage.addChild(image);
+
+
+var hawaii = PIXI.Sprite.fromImage('img/miles_w_mustache.png');
+ 
+hawaii.width=150;
+hawaii.height=150;
+
+//center the sprite anchor point
+hawaii.anchor.x = 0;
+hawaii.anchor.y = 0;
+ 
+// move the sprite to the center of the canvas
+hawaii.position.x = window.innerWidth/2 - hawaii.getBounds().width/2;
+hawaii.position.y = window.innerHeight/2.5 - hawaii.getBounds().height/2;
+
+ 
+app.stage.addChild(hawaii);
